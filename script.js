5f96e91d36f062d45026334e6c1928baa246b4fa
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-30 20:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const cards= document.querySelectorAll(".card");
+const myCarousel= document.querySelector("#myCarousel");
+const myCarouselInner= document.querySelector(".carousel-inner");
+const myCarouselItems= document.querySelectorAll(".carousel-item");
+
+
+cards.forEach(card => {
+    card.addEventListener("click", e => {
+        myCarouselItems.forEach(item => {
+            if(item.classList.contains(e.currentTarget.id)) {
+                console.log(item);
+                item.classList.add("active");
+            }
+        })
+
+        myCarousel.classList.remove("d-none");
+        myCarousel.classList.add("d-block");
+    })
+})
+
+myCarousel.addEventListener("click", e => {
+    if(e.target===myCarousel || e.target===myCarouselInner) {
+        myCarousel.classList.add("d-none");
+        myCarousel.classList.remove("d-block");
+        myCarouselItems.forEach(item => {
+            item.classList.remove("active");
+        })
+    }
+})
